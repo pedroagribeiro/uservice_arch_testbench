f695b0a2f54d4350777e665adee134e9ecbfa259
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2022-02-03 09:58:27 +0000

    :heavy_plus_sign: Adds RabbitMQ

--- producer/src/main/java/Producer.java
+++ producer/src/main/java/Producer.java
@@ -1,18 +1,34 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.nio.charset.StandardCharsets;
 import java.util.Random;
 
 public class Producer {
 
     private static int olt_number = 100;
     private static int messages_to_generate = 10000;
+    private final static String QUEUE_NAME = "message_queue";
 
     public static void main(String[] args) throws Exception {
-        for(int i = 0; i < messages_to_generate; i++) {
-            Random rand = new Random();
-            Message m = message_generator();
-            System.out.println(m);
-            // Sleep Time: 100 < t < 1000
-            int sleep_time = (rand.nextInt(10) + 1) * 100;
-            Thread.sleep(sleep_time);
+
+        // Create a connection to the RabbitMQ server
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            for(int i = 0; i < messages_to_generate; i++) {
+               Random rand = new Random();
+               Message m = message_generator();
+               // Sleep Time: 100 < t < 1000
+               int sleep_time = (rand.nextInt(10) + 1) * 100;
+               channel.basicPublish("", QUEUE_NAME, null, m.get_olt().getBytes());
+               System.out.println("[x] Sent '" + m.get_olt() + "'");
+               Thread.sleep(sleep_time);
+            }
         }
     }
 
